4c4d5ad6cab82d8b121cb481a52001a45c02436e
Bang DAI <bang.dai2@gmail.com>
2023-02-11 17:15:45 +0100

    stable test

--- backend/test/unit/02-NFTCollectionFactory.js
+++ backend/test/unit/02-NFTCollectionFactory.js
@@ -94,24 +94,99 @@ const { developmentChains } = require("../../helper-hardhat-config")
 
             it("Should transfert NFT to buyer and the money to seller", async function () {
                 const price = "10"; //10eth
+                // const contractBalance = await NFTMarketPlace.getBalance()
+                // console.log(contractBalance)
+
                 //owner paid gas fee
                 const tokenId = await createNFT(5)
                 const tx = await NFTMarketPlace.listNFT(tokenId, ethers.utils.parseEther(price), NFTCollectionAddr) //convert price to wei
                 await tx.wait()
 
-                //owner balance is now clean
+                //owner balance is now clean because he already paid fee
                 const sellerOldBalance = await deployer.getBalance()
                 const transaction = await NFTMarketPlace.connect(user1).buyNFT(tokenId, NFTCollectionAddr, { value: ethers.utils.parseEther(price) })
+
                 //check event
                 await expect(transaction).to.emit(NFTMarketPlace, "NFTTransfer").withArgs(tokenId, user1.address)
                 await transaction.wait()
+
                 //check seller receive money
+                const sellerProfit = price * 98 / 100 //seller receive 98% of price, because MP fee is 2%
                 const sellerNewBalance = await deployer.getBalance()
                 const profit = sellerNewBalance.sub(sellerOldBalance).toString()
-                assert.equal(profit, ethers.utils.parseEther(price).toString())
+                assert.equal(profit, ethers.utils.parseEther(sellerProfit.toString()).toString())
+
+                //check marketplace get 2% fees
+                // const plateformeFee = price - sellerProfit
+                // const newContractBalance = await NFTMarketPlace.getBalance()
+                // console.log(newContractBalance)
+
+                // const earn = newContractBalance.sub(contractBalance)
+                // console.log(earn)
+
+
                 //check if new owner has nft
                 const ownerAddr = await NFTCollection.ownerOf(tokenId)
                 assert.equal(ownerAddr, user1.address)
             })
         })
+
+        describe("CancelListing", function () {
+            it("Should revert if NFT is not listed", async function () {
+                const tx = NFTMarketPlace.cancelListing(1234, NFTCollectionAddr)
+                await expect(tx).to.be.revertedWith("NFT not listed for sale!");
+            })
+
+            it("Should revert if caller is not the seller", async function () {
+                const price = "10"; //10eth                
+                const tokenId = await createNFT(6)
+                const tx = await NFTMarketPlace.listNFT(tokenId, ethers.utils.parseEther(price), NFTCollectionAddr) //convert price to wei
+                await tx.wait()
+
+                const tx2 = NFTMarketPlace.connect(user1).cancelListing(tokenId, NFTCollectionAddr)
+                await expect(tx2).to.be.revertedWith("You are not the owner of this NFT!");
+            })
+
+            it("Should transfer ownership back to the seller if everyhing is ok", async function () {
+                //create and list NFT
+                const price = "10"; //10eth                
+                const tokenId = await createNFT(7)
+                const tx = await NFTMarketPlace.listNFT(tokenId, ethers.utils.parseEther(price), NFTCollectionAddr) //convert price to wei
+                await tx.wait()
+
+                //MarketPlace owner the NFT
+                const marketPlaceAddr = await NFTCollection.ownerOf(tokenId)
+                assert.equal(marketPlaceAddr, NFTMarketPlaceAddr)
+
+                //cancel listing
+                const tx2 = await NFTMarketPlace.cancelListing(tokenId, NFTCollectionAddr)
+                await expect(tx2).to.emit(NFTMarketPlace, "NFTCancel").withArgs(tokenId, deployer.address)
+
+                //check ownership is back
+                const ownerAddr = await NFTCollection.ownerOf(tokenId)
+                assert.equal(ownerAddr, deployer.address)
+            })
+
+        })
+
+        describe("Withdraw", function () {
+            it("Should revert if caller is not the owner", async function () {
+                const tx = NFTMarketPlace.connect(user1).withdraw()
+                await expect(tx).to.be.revertedWith("Ownable: caller is not the owner");
+            })
+
+            it("Should withdraw correctly to owner", async function () {
+                const deployerBalance = await deployer.getBalance()
+                const tx = await NFTMarketPlace.withdraw()
+                await tx.wait()
+                const deployerNewBalance = await deployer.getBalance()
+                expect(deployerNewBalance).to.be.greaterThan(deployerBalance)
+            })
+
+            it("Should revert if marketplace contract has empty balance", async function () {
+                const tx = NFTMarketPlace.withdraw()
+                await expect(tx).to.be.revertedWith("Empty balance");
+            })
+        })
+
     })
