1b83f88eac2f6363bb94fe191b65b50271983353
Bang DAI <bang.dai2@gmail.com>
2023-02-13 01:25:51 +0100

    add create nft page

---
+++ frontend/components/nft/CreateNFT.jsx
@@ -0,0 +1,66 @@
+import { Input, Text, Textarea, Card, CardHeader, CardBody, CardFooter, Stack, Heading, Button, Image, Flex, useToast } from '@chakra-ui/react';
+import { useRef, useState } from 'react';
+
+const CreateNFT = () => {
+    const toast = useToast()
+    const [isLoading, setLoading] = useState(false)
+    const [file, setFile] = useState("https://via.placeholder.com/150")
+
+    const handleCreate = async () => {
+        setLoading(true)
+        try {
+            //await addProposal(id)
+            toast({
+                description: "NFT créee avec succès.",
+                status: 'success',
+                isClosable: true,
+            })
+            URL.revokeObjectURL(file)
+        }
+        catch (e) {
+            toast({
+                description: e.error?.data?.message ?? "Une erreur inconnu s'est produite!",
+                status: 'error',
+                isClosable: true,
+            })
+        }
+
+        setLoading(false)
+    }
+
+    const handleChange = (e) => {
+        console.log(e.target.files[0])
+        setFile(URL.createObjectURL(e.target.files[0]))
+    }
+
+    return (
+        <Flex direction="column">
+            <Card
+                direction={{ base: 'column', sm: 'row' }}
+                overflow='hidden'
+                variant='outline'
+            >
+                <Image
+                    objectFit='cover'
+                    maxW={{ base: '100%', sm: '200px' }}
+                    src={file}
+                    alt='Caffe Latte'
+                />
+
+                <Stack>
+                    <CardHeader><Heading size='md'>Créer votre NFT</Heading></CardHeader>
+                    <CardBody>
+                        <Input placeholder='Image du NFT' type="file" onChange={handleChange} />
+                    </CardBody>
+                    <CardFooter>
+                        <Button variant='solid' colorScheme='blue' onClick={handleCreate} isLoading={isLoading}>
+                            Créer
+                        </Button>
+                    </CardFooter>
+                </Stack>
+            </Card>
+        </Flex>
+    );
+};
+
+export default CreateNFT;
--- frontend/pages/create.js
+++ frontend/pages/create.js
@@ -1,11 +1,12 @@
 import Layout from '@/components/layout/Layout';
+import CreateNFT from '@/components/nft/CreateNFT';
 import { Button } from '@chakra-ui/react';
 
 export default function Home() {
 
     return (
         <Layout>
-            page create
+            <CreateNFT />
         </Layout>
     )
 }
